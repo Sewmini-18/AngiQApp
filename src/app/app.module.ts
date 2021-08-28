8954aba465099a426e2520f2a3cc3befe172efae
Sewmini Dissanayaka <61307442+Sewmini-18@users.noreply.github.com>
2021-08-28 23:00:51 +0530

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent,
+    QuizComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
